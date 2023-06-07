d9e821b26306358af89f010229ed6d481280ced0
Бульбулятор <andrey.khlebov@bk.ru>
2023-06-07 22:14:07 +0300

    last

--- hotel/urls.py
+++ hotel/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.RoomsView.as_view()), 
+    path("", views.RoomsView.as_view(), name='home'), 
     path("<slug:slug>/", views.RoomDetailView.as_view(), name="room_detail"),
 ]
