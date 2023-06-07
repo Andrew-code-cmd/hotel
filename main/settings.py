2d186cae86a879611280e27e65dcf1ab4de8cd16
Бульбулятор <andrey.khlebov@bk.ru>
2023-06-07 18:00:47 +0300

    last

--- main/settings.py
+++ main/settings.py
@@ -26,8 +26,9 @@ SECRET_KEY = 'django-insecure-g_yfm@4@gohn6=c-8)a_d9=t5h#ej4mi$9bcetgu7ez90+sz&+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*", "https://web-production-e6ff4.up.railway.app"]
+ALLOWED_HOSTS = ["*"]
 
+CSRF_TRUSTED_ORIGINS = ['https://web-production-e6ff4.up.railway.app']
 
 # Application definition
 
